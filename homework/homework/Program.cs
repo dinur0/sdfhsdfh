a3b9898bcb16f3e61f6f33386256bb325b47a303
аывпфывпирывар <sxfgbvszdfb>
2023-02-05 14:18:04 +0300

    add space in text

--- homework/homework/Program.cs
+++ homework/homework/Program.cs
@@ -2,3 +2,5 @@
 string [] result = text.Where(a => a.Length <= 3).ToArray();
 System.Console.WriteLine(string.Join(" ", result));
 //jklhsdfgklghsdfjknjbjksdfnjbkohsdfuiobhjuiosdopb
+
+//this is first line of text
